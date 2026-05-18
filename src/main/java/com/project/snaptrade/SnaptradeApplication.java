5b0c4ee7f7fca686f361a2fa4abc6f75f7af3ffd
YBSeok <soberyl4304@gmail.com>
2026-05-18 18:28:12 +0900

    feat: init prj

---
+++ src/main/java/com/project/snaptrade/SnaptradeApplication.java
@@ -0,0 +1,13 @@
+package com.project.snaptrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaptradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaptradeApplication.class, args);
+	}
+
+}
